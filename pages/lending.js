217a17b004a74aaa12b472600f47e1f17c2ad093
stugufaka <stugufaka@gmail.com>
2023-03-21 15:29:42 +0000

    xdcpay

--- pages/lending.js
+++ pages/lending.js
@@ -1,14 +1,15 @@
 import Footer from "../components/Footer";
+import React, { useEffect, useState, useRef } from "react";
 import Header from "../components/Header";
 import PaymentComponent from "../components/payment";
 import SwapComponent from "../components/SwapComponent";
 
 import { useAccount } from "wagmi";
-const { address } = useAccount();
-
-console.log(address);
 
 export default function Home() {
+  const { address } = useAccount();
+  console.log(address);
+
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center bg-gradient-to-r from-blue-400 to-emerald-400">
       <Header />
